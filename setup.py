c95548047c3fef9ec78ebd4c402d6a08ee851786
Rick <henriqueyan@hotmail.com>
2025-03-28 15:24:55 -0300

    first full compilation

--- setup.py
+++ setup.py
@@ -1,23 +1,79 @@
+import glob
+from pathlib import Path
+import shlex
 from setuptools import setup
 from setuptools import Extension
-import numpy
+from setuptools.command.build_ext import build_ext
+import os
+import subprocess
 
+source_files = glob.glob("src/**/*.cpp", recursive=True) + glob.glob("src/**/*.cu", recursive=True)
+
+class CustomBuildExt(build_ext):
+    def run(self):
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        ext = self.extensions[0]
+        for source in ext.sources:
+            source = shlex.quote(source)
+            build_temp = shlex.quote(os.path.abspath(self.build_temp))
+            if source.endswith(".cu"):
+                compile_command = f"nvcc -allow-unsupported-compiler -O3 -DTESTING_STENCIL -lcudart -lcuda -Xcompiler -fPIC,-fopenmp -c {source} -odir {build_temp}"
+            elif source.endswith(".cpp"):
+                if 'wrapper' not in source:
+                    compile_command = f"nvcc -O3 -DTESTING_STENCIL -lcudart -lcuda  -Xcompiler -fPIC,-fopenmp -c \"{source}\" -odir {build_temp}"
+                else:  # wrapper needs Python.h and numpy/core
+                    
+                    import numpy as np
+                    include_paths = [
+                        np.get_include(),
+                        "/usr/include/python3.12",
+                        "src/rwnmr/utils",
+                        "src/rwnmr/math",
+                        "src/rwnmr/framework",
+                        "src/rwnmr/configFiles",
+                    ]
+
+                    self.include_dirs.extend(include_paths)
+                    compile_command = f"nvcc -O3 -DTESTING_STENCIL -lcudart -lcuda -Xcompiler -fPIC,-fopenmp -c {source} -odir {build_temp}" + \
+                        ' '.join([f" -I\"{include_dir}\"" for include_dir in self.include_dirs])
+            else:  # skip other files
+                continue
+            print(compile_command)
+            subprocess.check_call(compile_command, shell=True)
+        ext.sources = []
+        super().run()  
+
+    def build_extensions(self):
+        built_objects = glob.glob(os.path.join(self.build_temp, "*.o*"))
+        lib_filename = os.path.join(self.build_lib, self.ext_map['rwnmr']._file_name)
+        Path(os.path.split(lib_filename)[0]).mkdir(parents=True, exist_ok=True)
+        linker_command = rf"nvcc -Xcompiler -fopenmp -shared -o {lib_filename} {' '.join(built_objects)}" + \
+            ' '.join([f" -L{library_dir}" for library_dir in self.library_dirs]) 
+        print(linker_command)
+        subprocess.check_call(linker_command, shell=True)
+        super().build_extensions()
 
 setup(
     name="rwnmr",
-    version=1,
+    version=1.5,
     ext_modules=[
         Extension(
             "rwnmr",  # Nome do módulo
-            ['src/rwnmr/framework/rwnmr_wrapper.cpp','src/rwnmr/configFiles/CpmgConfig.cpp','src/rwnmr/configFiles/RwnmrConfig.cpp','src/rwnmr/configFiles/UctConfig.cpp', 'src/rwnmr/framework/BitBlock.cpp',], # Arquivos C
-            include_dirs=[numpy.get_include(), 'src/rwnmr/framework','/usr/include/eigen3', 'src/rwnmr/configFiles'],  # Inclua o diretório do seu cabeçalho Defs.h
-            extra_compile_args=['-std=c99'], # Adicione esta linha para usar o padrão C99
-            language="c++", # Adicione esta linha para usar o compilador C++
+            sources= source_files
+            ,
+            include_dirs=[
+                'src/rwnmr/framework',
+                '/usr/include/eigen3',
+                'src/rwnmr/configFiles',
+            ],
+            extra_compile_args=['-std=c++11'], 
+            language="c++",
         )
     ],
+    cmdclass={'build_ext': CustomBuildExt},
     install_requires=['numpy'],
     setup_requires=['numpy'],
-    # Adicione as linhas abaixo para garantir que o setuptools encontre o compilador C++
-    build_requires=['setuptools>=61.0'], # Ou a versão mais recente
 )
 #se não funcionar faça pip install --upgrade setuptools wheel 
